e34a63a24e73d36b334cc5b6e20baa037b02a67a
RystikS <rustamsagadeev@gmail.com>
2024-09-10 18:55:56 +0500

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,74 @@
+class House():
+  houses_history = []
+
+  def __new__(cls, *args):
+      return object.__new__(cls)
+
+  def __init__(self,name,number_of_floors):
+    self.name = name
+    self.number_of_floors = number_of_floors
+    House.houses_history.append(self.name)
+  def __del__(self):
+    print (f'{self.name} снесен, но он останется в истории')
+  def go_to(self,new_floor):
+    if new_floor > self.number_of_floors or new_floor < 1:
+        print("Такого этажа не существует")
+    else:
+        for i in range(1,new_floor+1):
+            print(i)
+  def __len__(self):
+      return self.number_of_floors
+  def __str__(self):
+      return (f'Название:{self.name},кол-во этажей: {self.number_of_floors}')
+  def __eq__(self, other):
+    if isinstance(other, House):
+        return self.number_of_floors == other.number_of_floors
+    else:
+        return False
+  def __lt__(self, other):
+      if isinstance(other, House):
+          return self.number_of_floors < other.number_of_floors
+      else:
+          return False
+  def __le__(self, other):
+      if isinstance(other, House):
+          return self.number_of_floors <= other.number_of_floors
+      else:
+          return False
+  def __gt__(self, other):
+      if isinstance(other, House):
+          return self.number_of_floors > other.number_of_floors
+      else:
+          return False
+  def __ge__(self, other):
+      if isinstance(other, House):
+          return self.number_of_floors >= other.number_of_floors
+      else:
+          return False
+  def __ne__(self, other):
+      if isinstance(other, House):
+          return self.number_of_floors != other.number_of_floors
+      else:
+          return False
+  def __add__(self, value):
+    if isinstance(value, int):
+      return House(self.name, self.number_of_floors + value)
+    else:
+      return self.number_of_floors + value
+  def __radd__(self, value):
+    return self.__add__(value)
+  def __iadd__(self, value):
+    return self.__add__(value)
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+print(House.houses_history)
